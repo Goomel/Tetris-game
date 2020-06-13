5c55014144bf6070d08f33f0e012f02faa376b01
Jakub Grzymisławski <thegomel10@gmail.com>
2020-06-13 15:25:40 +0200

    Add moving and rotating of the tetromino

--- app.js
+++ app.js
@@ -5,158 +5,9 @@ const rows = 20;
 const columns = 10;
 const sq = 20; //square size (20px x 20px)
 const vacant = "white"; //color of empty square
-
-//tetrominoes shapes
-const I = [
-    [
-        [0, 0, 0, 0],
-        [1, 1, 1, 1],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-    ],
-    [
-        [0, 0, 1, 0],
-        [0, 0, 1, 0],
-        [0, 0, 1, 0],
-        [0, 0, 1, 0],
-    ],
-    [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [1, 1, 1, 1],
-        [0, 0, 0, 0],
-    ],
-    [
-        [0, 1, 0, 0],
-        [0, 1, 0, 0],
-        [0, 1, 0, 0],
-        [0, 1, 0, 0],
-    ]
-];
-
-const J = [
-    [
-        [1, 0, 0],
-        [1, 1, 1],
-        [0, 0, 0]
-    ],
-    [
-        [0, 1, 1],
-        [0, 1, 0],
-        [0, 1, 0]
-    ],
-    [
-        [0, 0, 0],
-        [1, 1, 1],
-        [0, 0, 1]
-    ],
-    [
-        [0, 1, 0],
-        [0, 1, 0],
-        [1, 1, 0]
-    ]
-];
-
-const L = [
-    [
-        [0, 0, 1],
-        [1, 1, 1],
-        [0, 0, 0]
-    ],
-    [
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 1, 1]
-    ],
-    [
-        [0, 0, 0],
-        [1, 1, 1],
-        [1, 0, 0]
-    ],
-    [
-        [1, 1, 0],
-        [0, 1, 0],
-        [0, 1, 0]
-    ]
-];
-
-const O = [
-    [
-        [0, 0, 0, 0],
-        [0, 1, 1, 0],
-        [0, 1, 1, 0],
-        [0, 0, 0, 0],
-    ]
-];
-
-const S = [
-    [
-        [0, 1, 1],
-        [1, 1, 0],
-        [0, 0, 0]
-    ],
-    [
-        [0, 1, 0],
-        [0, 1, 1],
-        [0, 0, 1]
-    ],
-    [
-        [0, 0, 0],
-        [0, 1, 1],
-        [1, 1, 0]
-    ],
-    [
-        [1, 0, 0],
-        [1, 1, 0],
-        [0, 1, 0]
-    ]
-];
-
-const T = [
-    [
-        [0, 1, 0],
-        [1, 1, 1],
-        [0, 0, 0]
-    ],
-    [
-        [0, 1, 0],
-        [0, 1, 1],
-        [0, 1, 0]
-    ],
-    [
-        [0, 0, 0],
-        [1, 1, 1],
-        [0, 1, 0]
-    ],
-    [
-        [0, 1, 0],
-        [1, 1, 0],
-        [0, 1, 0]
-    ]
-];
-
-const Z = [
-    [
-        [1, 1, 0],
-        [0, 1, 1],
-        [0, 0, 0]
-    ],
-    [
-        [0, 0, 1],
-        [0, 1, 1],
-        [0, 1, 0]
-    ],
-    [
-        [0, 0, 0],
-        [1, 1, 0],
-        [0, 1, 1]
-    ],
-    [
-        [0, 1, 0],
-        [1, 1, 0],
-        [1, 0, 0]
-    ]
-];
+const startBtn = document.getElementById('start-btn');
+//it will be Piece class object
+let p;
 
 //creating a board and filling by vacant
 let board = [...Array(rows).fill([])];
@@ -196,6 +47,7 @@ const pieces = [
 
 const randomTetromino = () => {
     let random = Math.round((Math.random() * pieces.length));
+    //p variable become an instance of Piece class 
     return new Piece(pieces[random][0], pieces[random][1]);
 }
 
@@ -206,10 +58,10 @@ class Piece {
         this.currTetromino = 0;
         this.activeTetromino = this.tetromino[this.currTetromino];
         this.x = 3;
-        this.y = 0;
+        this.y = -2;
     }
 }
-const p = randomTetromino();
+
 Piece.prototype.fill = (color) => {
     for (let c = 0; c < p.activeTetromino.length; c++) {
         for (let r = 0; r < p.activeTetromino.length; r++) {
@@ -222,8 +74,51 @@ Piece.prototype.fill = (color) => {
 Piece.prototype.draw = () => {
     p.fill(p.color);
 }
-
-//falling down of tetromino
-Piece.prototype.moveDown = () => {
-
+Piece.prototype.undraw = () => {
+    p.fill(vacant);
 }
+Piece.prototype.moveDown = () => {
+    p.undraw();
+    p.y++;
+    p.draw();
+}
+Piece.prototype.rotate = () => {
+    p.undraw();
+    p.currTetromino < 3 ? p.currTetromino++ : p.currTetromino = 0;
+    p.activeTetromino = p.tetromino[p.currTetromino];
+    p.draw();
+}
+Piece.prototype.moveRight = () => {
+    p.undraw();
+    p.x++;
+    p.draw();
+}
+Piece.prototype.moveLeft = () => {
+    p.undraw();
+    p.x--;
+    p.draw();
+}
+
+
+window.addEventListener('keydown', (e) => {
+    if (e.keyCode == 37) {
+        p.moveLeft();
+    }
+    else if (e.keyCode == 38) {
+        p.rotate();
+    }
+    else if (e.keyCode == 39) {
+        p.moveRight();
+    }
+    else if (e.keyCode == 40) {
+        p.moveDown();
+    }
+})
+
+const startGame = () => {
+    p = randomTetromino();
+    window.setInterval(p.moveDown, 600);
+}
+
+startBtn.addEventListener('click', startGame)
+
